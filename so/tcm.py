4e1b86732d6c89aad23a272b26a1aa45abfa4c92
ubuntu <ubuntu@localhost.localdomain>
2019-03-13 08:58:19 +0800

    add tcm.py

---
+++ so/tcm.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import os
+
+class TcmPro:
+	def tcmPro(self, img_path, pro_type):
+		tmp = os.popen("./main "+img_path+" "+pro_type).readlines()
+		print(tmp)
+		tmp = tmp[0]
+		return tmp.replace("\n", "")
+
+
+	def facePro(self, img_path):
+		tmp = tcmPro(img_path, "face")
+		tmp = tmp.replace("tcmFaceFeature = ", "")
+		return tmp
+
+
+	def tongPro(self, img_path):
+		tmp = tcmPro(img_path, "tongue")
+		tmp = tmp.replace("tcmTongueFeature = ", "")
+		return tmp
+
+
+if __name__ == "__main__":
+	tcm = TcmPro()
+	print(tcm.facePro("pos1.jpg"))
+	print(tcm.tongPro("pos1.jpg"))
