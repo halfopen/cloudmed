4d377d0820e2496dd936dfa3aaa1aafd8929bccd
halfopen <halfopen@yeah.net>
2019-05-28 12:01:02 +0800

    雷达图描述文字超出的问题

--- 5plus/js/radar-option.js
+++ 5plus/js/radar-option.js
@@ -1,61 +1,43 @@
+
 var option = {
-    title: {},
-    tooltip: {},
+    title : {
+        // text: '预算 vs 开销（Budget vs spending）',
+        // subtext: '纯属虚构'
+    },
+    tooltip : {
+        // trigger: 'axis'
+    },
     legend: {
         data: ['体质得分'],
         tooltip:{
             color: 'orange'
-        }							
-    },
-    radar: {
-        // shape: 'circle',
-        name: {
-            textStyle: {
-                color: '#fff',
-                backgroundColor: '#999',
-                borderRadius: 3,
-                padding: [3, 5],
-                fontSize: 15,
-            }
-            
-        },
-        indicator: [
-            { name: '阳虚', max: 60},
-            { name: '阴虚', max: 60},
-            { name: '痰湿', max: 60},
-            { name: '瘀滞', max: 60},
-            { name: '脾虚', max: 60},
-            { name: '肾虚', max: 60},
-            { name: '气虚', max: 60}
-        ],
-        splitNumber: '2',
-        splitArea: {
-            areaStyle: {
-                color: ['white', 'white', 'white', 'white']
-            }
-        },
-        splitLine:{
-            lineStyle: {
-                color: ['#aaa', '#ddd'],
-                width: '1'
-            }
         }
         
     },
-    series: [{
-        name: '体质分数',
-        type: 'radar',
-        // areaStyle: {normal: {}},
-        data : [
-            {
-                value : [0,0,0,0,0,0,0],
-                name : '体质得分',
-                lineStyle:{
-                    color: 'darkred',
-                    width: '5'
-                }
-            },
-            
-        ]
-    }]
+    polar : [
+       {
+        indicator: [
+            { name: '阴虚', max: 50},
+            { name: '阳虚', max: 50},
+            { name: '气虚', max: 50},
+            { name: '血瘀', max: 50},
+            { name: '肾虚', max: 50},
+            { name: '瘀滞', max: 50}
+         ],
+        }
+    ],
+    calculable : true,
+    series : [
+        {
+            name: '体质得分',
+            type: 'radar',
+            data : [
+                {
+                    value : [0, 0, 0, 0, 0, 0],
+                    name : '体质得分'
+                },
+            ]
+        }
+    ]
 };
+                    
